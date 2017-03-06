3ac7a927b85f18b9d02c4986149772fd4bd97fa7
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-03-06 17:27:06 +0000

    Made all scss files partials to avoid node-sass generating css files

---
+++ index.js
@@ -0,0 +1,5 @@
+// Loaded by NPM on: import Fenrir from 'fenrir';
+
+exports.printMsg = function() {
+  console.log("No js to include yet. css is in: '~fenrir/vendor/assets/stylesheets/fenrir/[tools, awoken, utilities]'. Or '~fenrir' if you just need the variables and mixins.");
+}
--- lib/fenrir/version.rb
+++ lib/fenrir/version.rb
@@ -1,3 +1,3 @@
 module Fenrir
-  VERSION = "0.1.8.8"
+  VERSION = "0.1.9"
 end
