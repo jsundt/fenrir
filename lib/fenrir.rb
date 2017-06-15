ed48ab8372ccaad88ba481cde4632da272acf9bf
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-06-15 16:49:51 +0100

    Restructure for design system (#6)
    * Moved files css files around to make them match a bit better with the design system
    
    * Version Bump
    
    * Fixes to disableing certain fenrir features
    
    * Fix version bump
    
    * Removed a missing variable
    
    * Fixed a bug with the fallback of the background color state generator
    
    * Fixed body opacity being changed by default text styling
    
    * Removed text--body styling from body

--- lib/fenrir.rb
+++ lib/fenrir.rb
@@ -13,7 +13,7 @@ module Fenrir
       FileUtils.mkdir_p "app/assets/stylesheets/base"
 
       #Copy variable file
-      copy_file "../../vendor/assets/stylesheets/fenrir/_defaults.scss", "app/assets/stylesheets/base/config.scss"
+      copy_file "../../vendor/assets/stylesheets/fenrir/base/_default_config.scss", "app/assets/stylesheets/base/config.scss"
     end
   end #end generator
 end
--- lib/fenrir/version.rb
+++ lib/fenrir/version.rb
@@ -1,3 +1,3 @@
 module Fenrir
-  VERSION = "0.1.13"
+  VERSION = "0.1.24"
 end
